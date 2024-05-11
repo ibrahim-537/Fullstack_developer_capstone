585e91e7630aa8042aeee36fca129679bd0bf626
ibrahim-537 <222n1a0537@bitskurnool.edu.in>
2024-05-11 12:15:13 +0530

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -10,7 +10,7 @@ def initiate():
     ]
 
 car_make_instances = []
-    for data in car_make_data:
+for data in car_make_data:
         car_make_instances.append(CarMake.objects.create(name=data['name'],
                                                          description=data['description']))
 
@@ -50,6 +50,6 @@ car_make_instances = []
         # Add more CarModel instances as needed
     ]
 
-    for data in car_model_data:
+for data in car_model_data:
             CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
     
