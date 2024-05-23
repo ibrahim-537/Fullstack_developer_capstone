3016167e0b354e25715a8bca2e0d2f181f83ca2e
ibrahim-537 <222n1a0537@bitskurnool.edu.in>
2024-05-23 12:44:51 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,11 +31,11 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     ('https://u222n1a0537-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
-    'proxy.cognitiveclass.ai')
+     'proxy.cognitiveclass.ai')
 ]
 CSRF_TRUSTED_ORIGINS = [
     ('https://u222n1a0537-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
-    'proxy.cognitiveclass.ai')
+     'proxy.cognitiveclass.ai')
 ]
 
 
